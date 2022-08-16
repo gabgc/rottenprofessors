aa2fbea9f94aca624233c98285718efdd919a3db
Gabriel Gonzalez <gabgcdev@gmail.com>
2022-08-16 17:10:29 -0400

    Removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { Professor } from "@prisma/client";
 import type { NextPage } from "next";
-import { useState } from "react";
 import AddProfessorForm from "../components/addProfessorForm";
 import AddUniversityForm from "../components/addUniversityForm";
 import Navbar from "../components/navbar";
@@ -10,7 +8,6 @@ const Home: NextPage = () => {
     <div>
       <Navbar></Navbar>
       <AdminPanel></AdminPanel>
-      <AddUniversityForm></AddUniversityForm>
     </div>
   );
 };
@@ -19,6 +16,7 @@ const AdminPanel = () => {
   return (
     <div>
       <AddProfessorForm></AddProfessorForm>
+      <AddUniversityForm></AddUniversityForm>
     </div>
   );
 };
