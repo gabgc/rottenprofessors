d16ecdc04b542fcbededb5376c97fa1f2a758788
gabgc <gabriel.gonzalez30@upr.edu>
2022-09-13 13:47:29 -0400

    Added dockerized DB

---
+++ docker/generate_data.js
@@ -0,0 +1,30 @@
+const client = require("@prisma/client");
+const fs = require("fs");
+
+const data = JSON.parse(fs.readFileSync("./dummydata.json", "utf8"));
+
+const prisma = new client.PrismaClient();
+
+const main = async () => {
+  const uni = await prisma.university.create({
+    data: {
+      name: "University of Puerto Rico",
+      address: "123 A St",
+      city: "San Juan",
+      rating: 0,
+    },
+  });
+
+  for (var i = 0; i < data.length; i++) {
+    const prof = await prisma.professor.create({
+      data: {
+        firstName: data[i].firstname,
+        lastName: data[i].lastname,
+        universityId: uni.id,
+      },
+    });
+    console.log(prof.id);
+  }
+};
+
+main();
